0cc3e0c16a3ba2948b9bf9a42df0731cc26c87e7
DCIAL42 <daniel05cooke@gmail.com>
2024-11-07 19:57:33 +0000

    stuff before bootstrap

--- script.js
+++ script.js
@@ -11,3 +11,23 @@ function hideContact() {
     document.getElementById("contact").style.height = "40%";
     document.getElementById("contact").style.display = "none";
 }
+
+let sections = [document.getElementById("home"),document.getElementById("about"),document.getElementById("work"),document.getElementById("images"),document.getElementById("contact-button")];
+let navLi = document.querySelectorAll("nav ul li");
+
+window.onscroll = () => {
+    console.log("af");
+    var current = "";
+    sections.forEach((section) => {
+        const sectionTop = section.offsetTop;
+        if (pageYOffset >= sectionTop ) {
+            current = section.getAttribute("id");
+        }
+    });
+    navLi.forEach((li) => {
+        li.classList.remove("active");
+        if (li.classList.contains(current)) {
+            li.classList.add("active");
+        }
+    });
+};
